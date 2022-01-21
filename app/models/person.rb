84e1818377966edbf1905568adc290cb19f1c4fa
Cosmel Villalobos <cosmelvg@gmail.com>
2022-01-21 00:45:51 -0500

    added the association for active storage and method to extract the image url

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,8 @@
 class Person < ApplicationRecord
+  include Rails.application.routes.url_helpers
+  has_one_attached :avatar
+
+  def avatar_image_url
+    url_for(avatar) if avatar.attached?
+  end
 end
